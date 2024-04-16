f143549f29c624a117e9e43d3b8be29ba78e8caf
Mustafa Tuna <mtuna61@gmail.com>
2024-04-16 19:04:32 +0100

    feat: initialize backend

---
+++ backend/src/main/java/com/tunayev/turkdil/TurkdilApplication.java
@@ -0,0 +1,13 @@
+package com.tunayev.turkdil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurkdilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurkdilApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tunayev/turkdil/TurkdilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tunayev.turkdil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurkdilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
