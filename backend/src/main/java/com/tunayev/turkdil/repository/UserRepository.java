ca2a4ad05f1341e421d48d85a2e24b109e5213dd
Mustafa Tuna <mtuna61@gmail.com>
2024-04-24 00:19:13 +0200

    feat: jjwt dependency added

--- backend/src/main/java/com/tunayev/turkdil/repository/UserRepository.java
+++ backend/src/main/java/com/tunayev/turkdil/repository/UserRepository.java
@@ -3,6 +3,11 @@ package com.tunayev.turkdil.repository;
 import com.tunayev.turkdil.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByActivationCode(String code);
 
 }
---
+++ backend/src/main/java/com/tunayev/turkdil/services/JwtService.java
@@ -0,0 +1,14 @@
+package com.tunayev.turkdil.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractEmail(String token) {
+        return null;
+    }
+
+    public String extractUsername(String token) {
+        return null;
+    }
+}
